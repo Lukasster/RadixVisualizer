51395dc56460328577c9c1911632ee62e74fa9b1
Lukas Sterbenz <sterbenz@student.tugraz.at>
2024-07-01 19:44:23 +0200

    added analytics

--- radix/src/app/page.tsx
+++ radix/src/app/page.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import QuickSortVisualizer from "../pages/quicksort";
 import RadixSortVisualization from "../pages/radix-sort-visualization";
 import NavBar from './components/navbar';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
   const [page, setPage] = useState('quicksort');
